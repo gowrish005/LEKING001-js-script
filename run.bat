08b11d72184932352efe376068fdb9ea694a6f8c
gowrish005 <gowrishhb5@gmail.com>
2025-03-11 18:27:51 +0530

    Initial commit

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+start /min "zaphnathpaaneah" cmd /c "node 1/bot.js"
+start /min "_nukul_tambi_" cmd /c "node 2/bot.js"
+start /min "MostlikelyLee" cmd /c "node 3/bot.js"
+start /min "DISHANT_MALVIYA" cmd /c "node 4/bot.js"
+start /min "LEKING001" cmd /c "node 5/bot.js"
